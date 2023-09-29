3f1512e0010420330f1a394ac863758fc9451c7e
Jonghyun Kim <kim@jonghyun.me>
2023-09-30 02:10:18 +0900

    refactor: save letters before useEffect on typingAnimateLetters

--- src/components/TypingAnimateLetters.tsx
+++ src/components/TypingAnimateLetters.tsx
@@ -7,22 +7,23 @@ const TYPING_DELAY = 8;
 export default function TypingAnimatedLetters({start, children, onDone}: PropsWithChildren<{start: boolean, onDone?: () => void}>) {
     const lettersRef = useRef(children!.toString());
     const [typingLetters, setTypingLetters] = useState('');
+    const letters = lettersRef.current;
 
     useEffect(() => {
         if (!start) {
             return;
         }
 
-        if (typingLetters.length >= lettersRef.current.length && onDone) {
+        if (typingLetters.length >= letters.length && onDone) {
             onDone();
         } else if (typingLetters.length === 0) {
-            setTypingLetters(lettersRef.current[0]);
+            setTypingLetters(letters[0]);
         } else {
             setTimeout(() => {
-                setTypingLetters(lettersRef.current.slice(0, typingLetters.length + 1));
+                setTypingLetters(letters.slice(0, typingLetters.length + 1));
             }, TYPING_DELAY);
         }
-    }, [typingLetters, start, onDone]);
+    }, [typingLetters, letters, start, onDone]);
 
     return <>{typingLetters}</>;
 }
